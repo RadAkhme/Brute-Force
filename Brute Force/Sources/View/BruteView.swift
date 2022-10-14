a9824b434fd3031a9cecdc457c1e142da2fcbf95
RadikAkhme <a.radik.s@ya.ru>
2022-10-14 20:21:41 +0300

    Add BruteView file

---
+++ Brute Force/Sources/View/BruteView.swift
@@ -0,0 +1,32 @@
+//
+//  BruteView.swift
+//  Brute Force
+//
+//  Created by Радик Ахметзянов on 14.10.2022.
+//
+
+import UIKit
+
+class BruteView: UIView {
+
+    // MARK: - Initial
+    
+    override init(frame: CGRect) {
+            super.init(frame: frame)
+            setupHierarchy()
+            setupStackView()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("Error")
+        }
+        
+        private func setupHierarchy() {
+            
+        }
+        
+        private func setupStackView() {
+        
+        }
+
+}
