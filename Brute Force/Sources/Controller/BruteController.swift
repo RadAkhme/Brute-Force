2306f18e99a07cfa121598d896d83b6d8ffa4edc
RadikAkhme <a.radik.s@ya.ru>
2022-10-17 23:52:18 +0300

    Button logic

--- Brute Force/Sources/Controller/BruteController.swift
+++ Brute Force/Sources/Controller/BruteController.swift
@@ -8,14 +8,35 @@
 import UIKit
 
 class BruteController: UIViewController {
-
+    
+    var brute = Brute()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let view = BruteView()
         self.view = view
+        view.delegate = self
         view.backgroundColor = .systemYellow
     }
-
-
 }
 
+extension BruteController: BruteViewDelegate {
+    func buttonPressed(_ bruteView: BruteView) {
+        bruteView.bgColor.toggle()
+    }
+    
+    func passwordButtonPressed(_ bruteView: BruteView) {
+        bruteView.textField.isSecureTextEntry = true
+        bruteView.label.text = "Сейчас угадаю..."
+        brute.randomPassword(bruteView)
+        bruteView.activityIndicator.startAnimating()
+        
+        if let password = bruteView.textField.text {
+            let queue = DispatchQueue(label: "", qos: .userInteractive)
+            
+            queue.async {
+                self.brute.bruteForce(bruteView, passwordToUnlock: password)
+            }
+        }
+    }
+}
